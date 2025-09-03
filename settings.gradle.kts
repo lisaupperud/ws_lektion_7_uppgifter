fd77a73899f65f024e0a864b99cf6ba2439ce678
liluppis <lisa.upp@hotmail.com>
2025-09-03 11:53:57 +0200

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "lektion_7_uppgifter"
---
+++ src/main/java/com/lisa/lektion_7_uppgifter/Lektion7UppgifterApplication.java
@@ -0,0 +1,13 @@
+package com.lisa.lektion_7_uppgifter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lektion7UppgifterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lektion7UppgifterApplication.class, args);
+	}
+
+}
